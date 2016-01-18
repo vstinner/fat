4e16a441c22e0bb89d11398be411276fb653cf05
Victor Stinner <vstinner@redhat.com>
2016-01-18 10:35:04 +0100

    fix runtests.sh for release mode

--- runtests.sh
+++ runtests.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e -x
 
+rm -rf build/
 PYTHON=~/prog/python/fatpython/python
 $PYTHON setup.py build
-PYTHONPATH=build/lib.linux-x86_64-3.6-pydebug/ $PYTHON test_fat.py
+PYTHONPATH=$(ls -d build/lib.linux-x86_64-3.6*/) $PYTHON test_fat.py
